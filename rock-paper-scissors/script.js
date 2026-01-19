c5ce703e36405ec9ef63e598abf6a836e36cf023
oscarvictiria <oscarvictoriagonzalez@gmail.com>
2026-01-19 00:01:02 -0500

    addded rock, paper, scissors

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,11 @@
+var choices = ['rock', 'paper', 'scissors']
+
+function playRound(value) {
+
+    if (value === 'rock') {
+        console.log('rock!'); 
+    } else {
+        console.log('Not a real value'); 
+    }
+
+}
