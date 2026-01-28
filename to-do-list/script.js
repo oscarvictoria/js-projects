3eda87af9b13db60b9363eb16312910b8f14ee48
oscarvictiria <oscarvictoriagonzalez@gmail.com>
2026-01-27 21:13:43 -0500

    updated functions to load UI

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -19,32 +19,30 @@ var list = document.querySelector('.item-list')
 var clearAll = document.querySelector('.clear')
 
 
-
-
 submit.addEventListener('click', submitClicked)
 
 clearAll.addEventListener('click', clearClicked)
 
-
-
-
 function submitClicked(e) {
+      e.preventDefault()
     if(newItem.value === '') {
         alert('Empty Value')
         return 
     }
 
-    e.preventDefault()
+  
     addToArray();
-     renderUI()
+    renderUI()
    
     console.log(newItem.value); 
     console.log(itemsArray)
 }
 
 function clearClicked(e) {
-    //  e.preventDefault()
-     itemsContainer.innerHTML = ''
+
+     itemsArray = []
+     renderUI()
+    
 
 }
 
@@ -54,6 +52,7 @@ function addToArray() {
 }
 
 function renderUI() {
+    list.innerHTML = ''
 
     if(itemsArray.length > 0) {
           list.style.display = "block"
@@ -61,27 +60,21 @@ function renderUI() {
        
     } else {
           list.style.display = "none"
-          clearAll.style.display = 'none'
-         
-  
-       
-         
+          clearAll.style.display = 'none'  
     }
+
+   
+
+    for(let i = 0; i < itemsArray.length; i++) {
+      var item = document.createElement('li'); 
+      item.textContent = itemsArray[i];
+      list.appendChild(item);
+     
+    }
+
+     
+      newItem.value = ''
  
 
-    var item = document.createElement('li'); 
-
-    item.textContent = newItem.value; 
-
-    list.appendChild(item); 
-
-    newItem.value = ''
-
-
-
-  
-
-
-
-
+    
 }
