1533c4a63778d14778cf361c7ddf35a5c7acf2a7
oscarvictiria <oscarvictoriagonzalez@gmail.com>
2026-01-27 19:47:25 -0500

    added to-do list project

---
+++ to-do-list/script.js
@@ -0,0 +1,80 @@
+// Item shape: string 
+
+// State variables: Bool
+
+// Array name: itemsArray
+
+// Storage key name: items
+
+var itemsArray = []; 
+
+var newItem = document.querySelector('.item'); 
+
+var submit = document.querySelector('.submit'); 
+
+var itemsContainer = document.querySelector('.display-items'); 
+
+var list = document.querySelector('.item-list')
+
+var clearAll = document.querySelector('.clear')
+
+
+
+
+submit.addEventListener('click', submitClicked)
+
+clearAll.addEventListener('click', clearClicked)
+
+
+
+
+function submitClicked(e) {
+    e.preventDefault()
+    addToArray();
+     renderUI()
+   
+    console.log(newItem.value); 
+    console.log(itemsArray)
+}
+
+function clearClicked(e) {
+     e.preventDefault()
+
+}
+
+function addToArray() {
+    itemsArray.push(newItem.value)
+    
+}
+
+function renderUI() {
+
+    if(itemsArray.length > 0) {
+          list.style.display = "block"
+            clearAll.style.visibility = 'visible'
+       
+    } else {
+          list.style.display = "none"
+         
+  
+       
+         
+    }
+ 
+
+    var item = document.createElement('li'); 
+
+    item.textContent = newItem.value; 
+
+    list.appendChild(item); 
+
+    newItem.value = ''
+
+
+
+  
+
+
+
+
+}
